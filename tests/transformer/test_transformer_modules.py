2120ef800904f6d786b2315e1a0305833561a828
Viktor Kovryzhkin <vik.kovrizhkin@gmail.com>
2020-03-15 15:24:56 +0200

    transformer encoder

--- tests/transformer/test_transformer_modules.py
+++ tests/transformer/test_transformer_modules.py
@@ -5,7 +5,11 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-from time_series_experiments.transformer.modules import PositionWiseFeedForwardNetwork
+from time_series_experiments.transformer.modules import (
+    PositionWiseFeedForwardNetwork,
+    TransformerEncoder,
+)
+from time_series_experiments.transformer.layers import PositionalEncoding
 
 from time_series_experiments.utils import get_initializer
 from time_series_experiments.utils import rmse
@@ -56,3 +60,40 @@ def test_position_wise_ffnn():
     assert np.all(np.isfinite(y_pred))
     error = rmse(y_pred, y_test)
     assert error < 0.5
+
+
+def test_transformer_encoder():
+    fdw = 28
+    fw = 7
+    attention_dim = 32
+    num_heads = 4
+
+    x_train, y_train, x_test, y_test = simple_seq_data(
+        nrows=1000, freq="1H", fdw=fdw, fw=fw, test_size=0.2
+    )
+
+    inputs = keras.Input(shape=(fdw, 1))
+    outputs, encoder_self_attention = TransformerEncoder(
+        attention_dim=attention_dim,
+        num_heads=num_heads,
+        attention_kernel_initializer=get_initializer("glorot_uniform", RANDOM_SEED),
+        pwffn_kernel_initializer=get_initializer("glorot_uniform", RANDOM_SEED),
+    )(inputs)
+    outputs = keras.layers.Flatten()(outputs)
+    outputs = keras.layers.Dense(
+        fw,
+        kernel_initializer=get_initializer("glorot_uniform", RANDOM_SEED),
+        activation="linear",
+    )(outputs)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    model.compile(
+        optimizer=keras.optimizers.Adam(0.01), loss=keras.losses.MeanSquaredError()
+    )
+
+    model.fit(x_train, y_train, epochs=5, batch_size=32, shuffle=False)
+
+    y_pred = model.predict(x_test)
+    assert np.all(np.isfinite(y_pred))
+    error = rmse(y_pred, y_test)
+    assert error < 2.0
--- time_series_experiments/transformer/modules.py
+++ time_series_experiments/transformer/modules.py
@@ -1,5 +1,7 @@
 from tensorflow import keras
 
+from .layers import MultiHeadAttention
+
 
 class PositionWiseFeedForwardNetwork(object):
     def __init__(
@@ -27,3 +29,46 @@ class PositionWiseFeedForwardNetwork(object):
         outputs = self.dense_1(inputs)
         outputs = self.dense_2(outputs)
         return outputs
+
+
+class TransformerEncoder(object):
+    def __init__(
+        self,
+        attention_dim,
+        num_heads,
+        dff=None,
+        attention_kernel_initializer="glorot_uniform",
+        pwffn_kernel_initializer="glorot_uniform",
+        layer_norm_epsilon=0.001,
+        dropout_rate=0.0,
+    ):
+        self.mha = MultiHeadAttention(
+            attention_dim=attention_dim,
+            num_heads=num_heads,
+            kernel_initializer=attention_kernel_initializer,
+        )
+
+        self.pwffn = PositionWiseFeedForwardNetwork(
+            d_model=attention_dim * num_heads,
+            dff=dff or attention_dim * num_heads * 4,
+            kernel_initializer=pwffn_kernel_initializer,
+        )
+
+        self.layernorm1 = keras.layers.LayerNormalization(epsilon=layer_norm_epsilon)
+        self.layernorm2 = keras.layers.LayerNormalization(epsilon=layer_norm_epsilon)
+
+        self.dropout1 = keras.layers.Dropout(dropout_rate)
+        self.dropout2 = keras.layers.Dropout(dropout_rate)
+
+    def __call__(self, inputs, mask=None):
+        outputs, encoder_self_attention = self.mha([inputs, inputs, inputs], mask=mask)
+        outputs = self.dropout1(outputs)
+        outputs = keras.layers.add([inputs, outputs])
+        outputs = self.layernorm1(outputs)
+
+        ffn_outputs = self.pwffn(outputs)
+        ffn_outputs = self.dropout2(ffn_outputs)
+        outputs = keras.layers.add([outputs, ffn_outputs])
+        outputs = self.layernorm2(outputs)
+
+        return outputs, encoder_self_attention
