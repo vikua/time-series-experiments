a4cba505fbad099d19fc9f1f6699c2f3ac0f9dc8
Viktor Kovryzhkin <vik.kovrizhkin@gmail.com>
2020-05-30 17:43:29 +0300

    nbeats air passengers example

---
+++ time_series_experiments/nbeats/__init__.py
@@ -0,0 +1,13 @@
+from .blocks import BlockTypes
+from .stacks import StackDef, StackTypes
+from .models import NBEATS, NBEATSLastForward, NBEATSResidual
+
+
+__all__ = [
+    "BlockTypes",
+    "StackDef",
+    "StackTypes",
+    "NBEATS",
+    "NBEATSLastForward",
+    "NBEATSResidual",
+]
